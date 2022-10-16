f70d578aff8d88f3e14f5917b34f740d7498d220
EvaWisse <101636039+EvaWisse@users.noreply.github.com>
2022-10-16 20:19:02 +0200

    Update exfunc.cpp

--- demo/cpa/hybrid/precomp/sign/exfunc.cpp
+++ demo/cpa/hybrid/precomp/sign/exfunc.cpp
@@ -5,7 +5,7 @@ void ECP2_toChar(char W[], ECP2 *P)
   FP2 qx, qy;
   ECP2_get(&qx, &qy, P);
   FP2_toBytes(&W[0],&qx);
-  FP2_toBytes(&(W[2 * MODBYTES_B256_28 + 1]), &qy);
+  FP2_toBytes(&(W[2 * MODBYTES_B256_28]), &qy);
 }
 
 void ECP_toChar(char W[], ECP *P)
@@ -14,7 +14,7 @@ void ECP_toChar(char W[], ECP *P)
   ECP_affine(P);
   ECP_get(x, y, P);
   BIG_toBytes(&(W[0]), x);
-  BIG_toBytes(&(W[MODBYTES_B256_28 + 1]), y);
+  BIG_toBytes(&(W[MODBYTES_B256_28]), y);
 }
 
 int dec_fromHex(char hex)
@@ -23,19 +23,6 @@ int dec_fromHex(char hex)
   else if (hex >= 'a' && hex <= 'f') return (int)(hex - 97 + 10);
   else return (int)(hex - 66 + 10);
 }
-
-// void FP2_fromBytes(FP2 *x,char *b)
-// {
-//     FP_fromBytes(&(x->b),b);
-//     FP_fromBytes(&(x->a),&b[MODBYTES_B256_28]);
-// }
-
-// void FP_fromBytes(FP *x,char *b)
-// {
-//     BIG t;
-//     BIG_fromBytes(t, b);
-//     FP_nres(x, t);
-// }
 
 void BIG_fromChar(BIG *a, char ch[])
 {
