e7de8d3094187574b71eb7f8a14cad11cc379562
EvaWisse <101636039+EvaWisse@users.noreply.github.com>
2022-10-16 20:22:01 +0200

    Update exfunc.h

--- demo/cpa/hybrid/original/sign/exfunc.h
+++ demo/cpa/hybrid/original/sign/exfunc.h
@@ -21,9 +21,10 @@ using namespace core;
 using namespace B256_28;
 using namespace BN254;
 
-#define ecp_size 66
-#define ecp2_size 129
+#define ecp_size 64
+#define ecp2_size 128
 #define big_size 32
+#define m_size 32
 
 void ECP2_toChar(char W[], ECP2 *P);
 void ECP_toChar(char W[], ECP *P);
